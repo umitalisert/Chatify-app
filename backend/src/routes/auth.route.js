570f00283358ae2738aa402d51fa40271faafd62
umitalisert <umitalisert@gmail.com>
2025-11-01 02:24:41 +0300

    ready for deployment

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/signup");
+router.get("/signup", (req, res) => res.send("Hello from signup route"));
 router.get("/login");
 router.get("/logout");
 router.get("/update");
